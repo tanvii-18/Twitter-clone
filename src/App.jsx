b1b55d7693832b43efa6b3178c3f1c2105589348
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-24 19:20:11 +0530

    24-11-25

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,48 @@ import "./index.css";
 function App() {
   return (
     <>
-      <h1>Twitter clone</h1>
+      <div className="h-screen w-full flex">
+        {/* left side menu part */}
+        <div className="h-screen w-80 border-r border-gray-400">
+          <div>
+            <div className="logo">
+              <img src="" alt="" />
+            </div>
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Explore</a>
+              </li>
+              <li>
+                <a href="">Notifications</a>
+              </li>
+              <li>
+                <a href="">Messages</a>
+              </li>
+              <li>
+                <a href="">Bookmarks</a>
+              </li>
+              <li>
+                <a href="">Lists</a>
+              </li>
+              <li>
+                <a href="">Profile</a>
+              </li>
+              <li>
+                <a href="">More</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/* middle part main-content */}
+        <div className="h-auto w-150"></div>
+
+        {/* right side news part */}
+        <div className="h-screen w-80 border-l border-gray-400"></div>
+      </div>
     </>
   );
 }
