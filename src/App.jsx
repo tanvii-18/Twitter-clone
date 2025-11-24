c015953b89914e40516c215ca3097f4c60d0b479
tanvii-18 <8438tanvipatel@gmail.com>
2025-11-24 20:03:31 +0530

    .....

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,11 @@
 import "./index.css";
+import Menu from "./screens/menu";
 
 function App() {
   return (
     <>
       <div className="h-screen w-full flex">
-        {/* left side menu part */}
-        <div className="h-screen w-80 border-r border-gray-400">
-          <div>
-            <div className="logo">
-              <img src="" alt="" />
-            </div>
-            <ul>
-              <li>
-                <a href="">Home</a>
-              </li>
-              <li>
-                <a href="">Explore</a>
-              </li>
-              <li>
-                <a href="">Notifications</a>
-              </li>
-              <li>
-                <a href="">Messages</a>
-              </li>
-              <li>
-                <a href="">Bookmarks</a>
-              </li>
-              <li>
-                <a href="">Lists</a>
-              </li>
-              <li>
-                <a href="">Profile</a>
-              </li>
-              <li>
-                <a href="">More</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Menu />
 
         {/* middle part main-content */}
         <div className="h-auto w-150"></div>
